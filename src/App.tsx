bb0feed00f523d9abccc70eaf8cc5788e0f0ffb1
Andrew Ingombe <andrew.zm@bptn.com>
2022-08-17 04:51:56 +0200

    style the home page container.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Home from "./pages/home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 
 function App() {
   return (
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import styled from "styled-components";
+import Calculator from "../components/Calculator";
+
+const Container = styled.div`
+  display: flex;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+  background-color: #eee;
+`;
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Calculator />
+    </Container>
+  );
 };
 
 export default Home;
