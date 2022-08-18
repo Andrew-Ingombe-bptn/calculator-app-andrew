fb9286e5effadbd73a7b47a8949fc5ad30f14512
Andrew Ingombe <andrew.zm@bptn.com>
2022-08-18 10:42:59 +0200

    tweak calculator wrapper.

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -4,13 +4,14 @@ import Display from "./Display";
 
 const CalculatorWrapper = styled.div`
   width: 100%;
-  max-width: 380px;
+  max-width: 300px;
   margin: 16px;
   border-radius: 5px;
   background-color: #fff;
   overflow: hidden;
   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
   padding: 16px;
+  border: 2px solid #009688;
 `;
 
 type CalculatorProps = {
@@ -28,7 +29,7 @@ const Calculator = ({
 }: CalculatorProps) => {
   return (
     <CalculatorWrapper>
-      <Display />
+      <Display currValue={currValue} />
       <Pad
         selectOperation={selectOperation}
         selectDigit={selectDigit}
