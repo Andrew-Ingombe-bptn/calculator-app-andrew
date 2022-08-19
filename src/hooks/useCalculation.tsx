3f8c9195374e897379e2e6e7f4760ed3ef3f3838
Andrew Ingombe <andrew.zm@bptn.com>
2022-08-19 11:40:19 +0200

    store state values in localstorage

--- src/hooks/useCalculation.tsx
+++ src/hooks/useCalculation.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 export const useCalculation = () => {
-  const [currValue, setCurrValue] = useState("0");
-  const [operation, setOperation] = useState("");
-  const [prevValue, setPrevValue] = useState("");
+  const [currValue, setCurrValue] = useState(
+    localStorage.getItem("currValue") || "0"
+  );
+  const [operation, setOperation] = useState(
+    localStorage.getItem("operation") || ""
+  );
+  const [prevValue, setPrevValue] = useState(
+    localStorage.getItem("prevValue") || ""
+  );
   const [overWrite, setOverWrite] = useState(true);
 
   // calculate function
@@ -29,12 +35,19 @@ export const useCalculation = () => {
         break;
     }
 
+    // clear state values from local storage
+
+    localStorage.setItem("currValue", "0");
+    localStorage.setItem("operation", "");
+    localStorage.setItem("prevValue", "");
+
     return result;
   };
 
   // equal button
   const equals = () => {
     const val = calculate();
+    localStorage.setItem("currValue", `${val}`);
     setCurrValue(`${val}`);
     setPrevValue("");
     setOperation("");
@@ -46,8 +59,10 @@ export const useCalculation = () => {
     if (currValue[0] === "0" && digit === "0") return;
     if (currValue.includes(".") && digit == ".") return;
     if (overWrite && digit !== ".") {
+      localStorage.setItem("currValue", digit);
       setCurrValue(digit);
     } else {
+      localStorage.setItem("currValue", `${currValue}${digit}`);
       setCurrValue(`${currValue}${digit}`);
     }
     setOverWrite(false);
@@ -57,12 +72,15 @@ export const useCalculation = () => {
   const selectOperation = (operation: string) => {
     if (prevValue) {
       const val = calculate();
+      localStorage.setItem("prevValue", `${val}`);
       setCurrValue(`${val}`);
       setPrevValue(`${val}`);
     } else {
+      localStorage.setItem("prevValue", currValue);
       setPrevValue(currValue);
     }
     setOperation(operation);
+    localStorage.setItem("operation", operation);
     setOverWrite(true);
   };
 
@@ -72,11 +90,18 @@ export const useCalculation = () => {
     setOperation("");
     setCurrValue("0");
     setOverWrite(true);
+
+    // clear state values from local storage
+
+    localStorage.setItem("currValue", "0");
+    localStorage.setItem("operation", "");
+    localStorage.setItem("prevValue", "");
   };
 
   // delete button
   const del = () => {
     setCurrValue("0");
+    localStorage.setItem("currValue", "0");
     setOverWrite(true);
   };
 
